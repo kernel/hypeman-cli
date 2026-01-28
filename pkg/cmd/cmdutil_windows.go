0ee9d29e8e7d5b9d363c87f64291e2c29e862743
David Meadows <dmeadows@stainless.com>
2026-01-28 15:32:29 -0500

    resolve custom code conflicts

--- pkg/cmd/cmdutil_windows.go
+++ pkg/cmd/cmdutil_windows.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //go:build windows
 
 package cmd
@@ -13,17 +12,3 @@ import (
 func createSocketPair() (*os.File, *os.File, bool, error) {
 	return nil, nil, false, errors.New("socket pairs not supported on Windows")
 }
-||||||| parent of fe526a1 (fix(cli): fix compilation on Windows)
-=======
-//go:build windows
-
-package cmd
-
-import "os"
-
-func streamOutputOSSpecific(label string, generateOutput func(w *os.File) error) error {
-	// We have a trick with sockets that we use when possible on Unix-like systems. Those APIs aren't
-	// available on Windows, so we fall back to using pipes.
-	return streamToPagerWithPipe(label, generateOutput)
-}
->>>>>>> fe526a1 (fix(cli): fix compilation on Windows)
