ac36a8a6b34f13645c9faa5763cc2af884c7786b
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2026-01-30 09:40:42 -0600

    remove erroneous merge conflict comments

--- pkg/cmd/cmdutil_windows.go
+++ pkg/cmd/cmdutil_windows.go
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-//go:build windows
-
-package cmd
-
-import (
-	"errors"
-	"os"
-)
-
-// createSocketPair is not supported on Windows, so we return an error
-// which causes createPagerFiles to fall back to using pipes.
-func createSocketPair() (*os.File, *os.File, bool, error) {
-	return nil, nil, false, errors.New("socket pairs not supported on Windows")
-}
-||||||| parent of fe526a1 (fix(cli): fix compilation on Windows)
-=======
 //go:build windows
 
 package cmd
@@ -26,4 +9,3 @@ func streamOutputOSSpecific(label string, generateOutput func(w *os.File) error)
 	// available on Windows, so we fall back to using pipes.
 	return streamToPagerWithPipe(label, generateOutput)
 }
->>>>>>> fe526a1 (fix(cli): fix compilation on Windows)
