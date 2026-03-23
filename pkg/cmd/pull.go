657b489926c6a8571608d707e6167303ed839682
Steven Miller <sjmiller609@gmail.com>
2026-03-23 15:40:01 -0400

    Preserve pull auto output

--- pkg/cmd/pull.go
+++ pkg/cmd/pull.go
@@ -2,7 +2,12 @@ package cmd
 
 import (
 	"context"
+	"fmt"
+	"os"
 
+	"github.com/kernel/hypeman-go"
+	"github.com/kernel/hypeman-go/option"
+	"github.com/tidwall/gjson"
 	"github.com/urfave/cli/v3"
 )
 
@@ -16,5 +21,50 @@ var pullCmd = cli.Command{
 }
 
 func handlePull(ctx context.Context, cmd *cli.Command) error {
-	return handleImageCreateLike(ctx, cmd, "hypeman pull <image>", "pull")
+	args := cmd.Args().Slice()
+	if len(args) < 1 {
+		return fmt.Errorf("image reference required\nUsage: hypeman pull <image>")
+	}
+
+	image := args[0]
+	params, malformedTags := buildImageNewParams(image, cmd.StringSlice("tag"))
+	for _, malformed := range malformedTags {
+		fmt.Fprintf(os.Stderr, "Warning: ignoring malformed tag: %s\n", malformed)
+	}
+
+	client := hypeman.NewClient(getDefaultRequestOptions(cmd)...)
+
+	var opts []option.RequestOption
+	if cmd.Root().Bool("debug") {
+		opts = append(opts, debugMiddlewareOption)
+	}
+
+	format := cmd.Root().String("format")
+	transform := cmd.Root().String("transform")
+
+	if format != "auto" {
+		var res []byte
+		opts = append(opts, option.WithResponseBodyInto(&res))
+		_, err := client.Images.New(ctx, params, opts...)
+		if err != nil {
+			return err
+		}
+		obj := gjson.ParseBytes(res)
+		return ShowJSON(os.Stdout, "pull", obj, format, transform)
+	}
+
+	fmt.Fprintf(os.Stderr, "Pulling %s...\n", image)
+
+	result, err := client.Images.New(ctx, params, opts...)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(os.Stderr, "Status: %s\n", result.Status)
+	if result.Digest != "" {
+		fmt.Fprintf(os.Stderr, "Digest: %s\n", result.Digest)
+	}
+	fmt.Fprintf(os.Stderr, "Image: %s\n", result.Name)
+
+	return nil
 }
